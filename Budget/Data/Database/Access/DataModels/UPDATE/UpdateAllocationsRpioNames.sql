6886f5e13eed251cb048979cc51acf2c4afd188a
KarmaScripter <terry.eppler@gmail.com>
2022-11-16 19:39:07 -0500

    removed unnecessary files;

--- Budget/Data/Database/Access/DataModels/UPDATE/UpdateAllocationsRpioNames.sql
+++
@@ -1,5 +0,0 @@
-UPDATE Allocations 
-INNER JOIN ResourcePlanningOffices 
-ON ResourcePlanningOffices.Code = Allocations.RpioCode 
-SET Allocations.RpioName = ResourcePlanningOffices.Name
-WHERE Allocations.RpioCode = ResourcePlanningOffices.Code;
--- Budget/Data/Database/Access/DataModels/UPDATE/UpdateAppropriationLevelAuthorityTreasuryData.sql
+++
@@ -1,7 +0,0 @@
-UPDATE Deobligations 
-INNER JOIN Funds 
-ON (Funds.BFY = Deobligations.BFY) 
-AND (Funds.Code = Deobligations.FundCode) 
-SET Deobligations.TreasurySymbol = Funds.TreasuryAccountCode, Deobligations.OmbAccountCode = Funds.OmbAccountCode, Deobligations.OmbAccountName = Funds.OmbAccountName
-WHERE Funds.Code = Deobligations.FundCode 
-AND Funds.BFY = Deobligations.BFY;
