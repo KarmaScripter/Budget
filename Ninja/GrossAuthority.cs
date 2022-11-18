f4dfdb61e0da2cf6d595b0d9f06b304289d281a4
KarmaScripter <terry.eppler@gmail.com>
2022-11-18 11:18:30 -0500

    added Data classes: GrossAuthority & GrossUtilization; added sql queries;

---
+++ Ninja/GrossAuthority.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file = "GrossAuthority.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
+// </copyright>
+
+namespace BudgetExecution
+{
+    public class GrossAuthority
+    {
+        public int ID { get; set; }
+
+        public Source Source { get; set; }
+
+        public GrossAuthority( )
+        {
+            
+        }
+    }
+}
---
+++ Ninja/GrossUtilization.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file = "GrossUtilization.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
+// </copyright>
+
+namespace BudgetExecution
+{
+    public class GrossUtilization
+    {
+        public int ID { get; set; }
+
+        public Source Source { get; set; }
+
+        public GrossUtilization( )
+        {
+            
+        }
+    }
+}
