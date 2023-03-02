fbfd360832ca05a29b787649b04796e0943b2d6a
KarmaScripter <terry.eppler@gmail.com>
2023-03-02 10:46:17 -0500

    up;

--- Ninja/CarryoverRequest.cs
+++ Ninja/CarryoverRequest.cs
@@ -4,6 +4,7 @@
 
 namespace BudgetExecution
 {
+    using System;
     using System.Collections.Generic;
     using System.Data;
     using System.Diagnostics.CodeAnalysis;
@@ -22,6 +23,78 @@ namespace BudgetExecution
         /// </value>
         public int ID { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Analysts.
+        /// </summary>
+        /// <value>
+        /// The Analyst
+        /// </value>
+        public string Analyst { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the DocumentTitle.
+        /// </summary>
+        /// <value>
+        /// The DocumentTitle.
+        /// </value>
+        public string DocumentTitle { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Amount.
+        /// </summary>
+        /// <value>
+        /// The Amount.
+        /// </value>
+        public double Amount { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Fund.
+        /// </summary>
+        /// <value>
+        /// The Fund
+        /// </value>
+        public string Fund { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Status
+        /// </summary>
+        /// <value>
+        /// The Status.
+        /// </value>
+        public string Status { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Original Request Date.
+        /// </summary>
+        /// <value>
+        /// The Original Request Date.
+        /// </value>
+        public DateOnly OriginalRequestDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Last Activity Date.
+        /// </summary>
+        /// <value>
+        /// The Last Activity Date.
+        /// </value>
+        public DateOnly LastActivityDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the BFS.
+        /// </summary>
+        /// <value>
+        /// The BFS.
+        /// </value>
+        public string BudgetFormulationSystem { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Comments.
+        /// </summary>
+        /// <value>
+        /// The Comments.
+        /// </value>
+        public string Comments { get; set; }
+        
         /// <summary>
         /// Gets or sets the source.
         /// </summary>
@@ -51,6 +124,7 @@ namespace BudgetExecution
         /// </summary>
         public CarryoverRequest( )
         {
+            Source = Source.CarryoverRequests;
         }
 
         /// <summary>
@@ -61,6 +135,18 @@ namespace BudgetExecution
         {
             Record = new DataBuilder( query ).Record;
             Data = Record.ToDictionary( );
+            ID = int.Parse( Record[ "CarryoverRequestsId" ].ToString(  ) ?? "0" );
+            Analyst = Record[ "Analyst" ].ToString(  );
+            DocumentTitle = Record[ "DocumentTitle" ].ToString(  );
+            Amount = double.Parse( Record[ "Amount" ].ToString(  ) ?? "0" );
+            Fund = Record[ "Fund" ].ToString(  );
+            Status = Record[ "Status" ].ToString(  );
+            BudgetFormulationSystem = Record[ "BudgetFormulationSystem" ].ToString(  );
+            Comments = Record[ "Comments" ].ToString(  );
+            OriginalRequestDate =
+                DateOnly.Parse( Record[ "OriginalRequestDate" ].ToString(  ) ?? "" );
+            LastActivityDate =
+                DateOnly.Parse( Record[ "LastActivityDate" ].ToString(  ) ?? "" );
         }
 
         /// <summary>
@@ -71,6 +157,18 @@ namespace BudgetExecution
         {
             Record = builder.Record;
             Data = Record.ToDictionary( );
+            ID = int.Parse( Record[ "CarryoverRequestsId" ].ToString(  ) ?? "0" );
+            Analyst = Record[ "Analyst" ].ToString(  );
+            DocumentTitle = Record[ "DocumentTitle" ].ToString(  );
+            Amount = double.Parse( Record[ "Amount" ].ToString(  ) ?? "0" );
+            Fund = Record[ "Fund" ].ToString(  );
+            Status = Record[ "Status" ].ToString(  );
+            BudgetFormulationSystem = Record[ "BudgetFormulationSystem" ].ToString(  );
+            Comments = Record[ "Comments" ].ToString(  );
+            OriginalRequestDate =
+                DateOnly.Parse( Record[ "OriginalRequestDate" ].ToString(  ) ?? "" );
+            LastActivityDate =
+                DateOnly.Parse( Record[ "LastActivityDate" ].ToString(  ) ?? "" );
         }
 
         /// <summary>
@@ -81,6 +179,18 @@ namespace BudgetExecution
         {
             Record = dataRow;
             Data = dataRow.ToDictionary( );
+            ID = int.Parse( dataRow[ "CarryoverRequestsId" ].ToString(  ) ?? "0" );
+            Analyst = dataRow[ "Analyst" ].ToString(  );
+            DocumentTitle = dataRow[ "DocumentTitle" ].ToString(  );
+            Amount = double.Parse(dataRow[ "Amount" ].ToString(  ) ?? "0" );
+            Fund = dataRow[ "Fund" ].ToString(  );
+            Status = dataRow[ "Status" ].ToString(  );
+            BudgetFormulationSystem = dataRow[ "BudgetFormulationSystem" ].ToString(  );
+            Comments = dataRow[ "Comments" ].ToString(  );
+            OriginalRequestDate =
+                DateOnly.Parse( dataRow[ "OriginalRequestDate" ].ToString(  ) ?? "" );
+            LastActivityDate =
+                DateOnly.Parse( dataRow[ "LastActivityDate" ].ToString(  ) ?? "" );
         }
     }
 }
