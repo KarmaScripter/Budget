a12e419cbcb468c44b40e5b6d6c992aa3f2da3fe
KarmaScripter <terry.eppler@gmail.com>
2023-03-10 08:55:50 -0500

    up

--- Controls/Loader/LoadingForm.cs
+++ Controls/Loader/LoadingForm.cs
@@ -19,13 +19,13 @@ namespace BudgetExecution
     {
         /// <summary> The image path </summary>
         public readonly string LoadingPath =
-            @"Resource\Images\Loader\Loading.gif";
+            @"C:\Users\terry\source\repos\Budget\Resource\Images\Loader\Loading.gif";
 
         public readonly string ProcessingPath =
-            @"Resource\Images\Loader\Processing.gif";
+            @"C:\Users\terry\source\repos\Budget\Resource\Images\Loader\Processing.gif";
 
         public readonly string WaitingPath =
-            @"Resource\Images\Loader\Waiting.gif";
+            @"C:\Users\terry\source\repos\Budget\Resource\Images\Loader\Waiting.gif";
 
         /// <summary> Gets or sets the picture. </summary>
         /// <value> The picture. </value>
