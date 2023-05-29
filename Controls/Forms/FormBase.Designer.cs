6e6e585099a01b9d2585b216ad9aa7f163aa166d
KarmaScripter <terry.eppler@gmail.com>
2023-05-29 18:20:21 -0400

    up;

--- Controls/Forms/FormBase.Designer.cs
+++ Controls/Forms/FormBase.Designer.cs
@@ -29,9 +29,13 @@
         private void InitializeComponent( )
         {
             components = new System.ComponentModel.Container( );
+            var bootstrap1 = new Zeroit.Framework.BasicCharts.Bootstrap( );
+            var resources = new System.ComponentModel.ComponentResourceManager( typeof( FormBase ) );
+            var material1 = new Zeroit.Framework.BasicCharts.Material( );
             ContextMenu = new ContextMenu( );
             BindingSource = new System.Windows.Forms.BindingSource( components );
             ToolTip = new SmallTip( );
+            zeroitBarChart1 = new Zeroit.Framework.BasicCharts.ZeroitBarChart( );
             ( (System.ComponentModel.ISupportInitialize) BindingSource  ).BeginInit( );
             SuspendLayout( );
             // 
@@ -69,6 +73,40 @@
             ToolTip.TipText = null;
             ToolTip.TipTitle = null;
             // 
+            // zeroitBarChart1
+            // 
+            zeroitBarChart1.AllowTransparency = true;
+            bootstrap1.Background = System.Drawing.Color.FromArgb(   35  ,   40  ,   50   );
+            bootstrap1.BorderWidth = 1F;
+            bootstrap1.GridColors = System.Drawing.Color.FromArgb(   60  ,   65  ,   70   );
+            bootstrap1.OrientBackground = System.Drawing.Color.FromArgb(   30  ,   35  ,   40   );
+            bootstrap1.TextColor = System.Drawing.Color.FromArgb(   115  ,   120  ,   125   );
+            zeroitBarChart1.BootstrapColors = bootstrap1;
+            zeroitBarChart1.DrawMode = Zeroit.Framework.BasicCharts.ZeroitBarChart.DrawType.Solid;
+            zeroitBarChart1.FilledColor = System.Drawing.Color.FromArgb(   30  ,   33  ,   38   );
+            zeroitBarChart1.FilledGradient = new System.Drawing.Color[ ] { System.Drawing.Color.Beige, System.Drawing.Color.Crimson };
+            zeroitBarChart1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            zeroitBarChart1.GraphOrientation = Zeroit.Framework.BasicCharts.ZeroitBarChart.Orientation.Vertical;
+            zeroitBarChart1.GraphStyle = Zeroit.Framework.BasicCharts.ZeroitBarChart.Style.Flat;
+            zeroitBarChart1.GridSize = 1F;
+            zeroitBarChart1.HatchBrush = new System.Drawing.Color[ ] { System.Drawing.Color.Black, System.Drawing.Color.Transparent };
+            zeroitBarChart1.HatchStyle = System.Drawing.Drawing2D.HatchStyle.BackwardDiagonal;
+            zeroitBarChart1.Items = (System.Collections.Generic.List<int>) resources.GetObject( "zeroitBarChart1.Items" ) ;
+            zeroitBarChart1.Location = new System.Drawing.Point( 144, 222 );
+            material1.Background = System.Drawing.Color.FromArgb(   40  ,   40  ,   40   );
+            material1.Colors = (System.Collections.Generic.List<System.Drawing.Color>) resources.GetObject( "material1.Colors" ) ;
+            zeroitBarChart1.MaterialColors = material1;
+            zeroitBarChart1.Name = "zeroitBarChart1";
+            zeroitBarChart1.ShowGrid = false;
+            zeroitBarChart1.Size = new System.Drawing.Size( 615, 302 );
+            zeroitBarChart1.Sorting = Zeroit.Framework.BasicCharts.ZeroitBarChart.SortStyle.Normal;
+            zeroitBarChart1.SplitterColor = System.Drawing.Color.FromArgb(   59  ,   62  ,   71   );
+            zeroitBarChart1.TabIndex = 1;
+            zeroitBarChart1.Text = "zeroitBarChart1";
+            zeroitBarChart1.TextAlignment = Zeroit.Framework.BasicCharts.ZeroitBarChart.Aligning.Far;
+            zeroitBarChart1.TextColor = System.Drawing.Color.FromArgb(   120  ,   120  ,   120   );
+            zeroitBarChart1.UnfilledColor = System.Drawing.Color.FromArgb(   37  ,   40  ,   49   );
+            // 
             // FormBase
             // 
             BackColor = System.Drawing.Color.FromArgb(   20  ,   20  ,   20   );
@@ -80,6 +118,7 @@
             CaptionFont = new System.Drawing.Font( "Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point );
             ClientSize = new System.Drawing.Size( 1338, 738 );
             ControlBox = false;
+            Controls.Add( zeroitBarChart1 );
             MaximizeBox = false;
             MaximumSize = new System.Drawing.Size( 1350, 750 );
             MetroColor = System.Drawing.Color.FromArgb(   20  ,   20  ,   20   );
@@ -99,5 +138,6 @@
         public SmallTip ToolTip;
         public System.Windows.Forms.BindingSource BindingSource;
         public ContextMenu ContextMenu;
+        private Zeroit.Framework.BasicCharts.ZeroitBarChart zeroitBarChart1;
     }
 }
